8f887790e1ec7879a9eb7a52f15700062504270a
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-03 20:13:50 -0300

    edit appJs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 const App =()=>{
-  return <h1>A REact App!</h1>
+  return <div>
+    <h2>Course Goals</h2>
+    <ul>
+      <li>learn</li>
+      <li>finish course</li>
+      <li>help other students</li>
+    </ul>
+  </div>
 }
 export default App;
