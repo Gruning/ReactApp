c9d39cf1e089e3f4dbea5d4bf4e2bce8f267afa4
AlbertoGruning-Ubuntu <alberto.gruning.zen@gmail.com>
2020-08-08 16:07:37 +0100

    modifay indexJS and css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-
+import GoalList from './components/GoalList'
+import './App.css'
 const App =()=>{
-  return <div>
+
+  const courSeGoals = [
+    {id:'cg1', text:'complete MERN course'},
+    {id:'cg2', text:'complete Net5 course'},
+    {id:'cg3', text:'complete Python course'},
+  ]
+
+  return(
+  <div className="course-goals">
     <h2>Course Goals</h2>
-    <ul>
-      <li>learn</li>
-      <li>finish course</li>
-      <li>help other students</li>
-    </ul>
+    <GoalList goals={courSeGoals}/>
   </div>
+  ) 
 }
 export default App;
